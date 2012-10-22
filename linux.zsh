5577c23b49d1c64f4ec1f8bef92241b66c9995ea
KyleHe <me@heshirui.com>
2012-10-22 22:33:47 +0800

    modified rvm PATH

--- linux.zsh
+++ linux.zsh
@@ -3,4 +3,4 @@ alias ls='ls --color=auto'
 source $ZSH/aliases.zsh
 export EDITOR=vim
 export TERM="xterm-256color"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+ATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
